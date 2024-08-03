dff0a5ccccac7ac41430d219453d2c7ff2a5b356
Marc Marcos <marcmarcosmadruga@gmail.com>
2024-08-03 16:33:54 +0200

    sync to change computers, don't remember

--- painting_blobs.py
+++ painting_blobs.py
@@ -23,11 +23,17 @@ pygame.font.init()
 
 last_checked = None
 
+draw_legend = True
+
 while is_running:
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_l:
+                draw_legend = not draw_legend
     
     # checking if there are new circles and drawing them
 
@@ -48,7 +54,30 @@ while is_running:
         usuarios_shapes[s].calculate_pos()
 
         usuarios_shapes[s].draw_itself(window_surface)
+    
+    # draw the legend
 
+    if draw_legend:
+        '''
+        my_font = pygame.font.SysFont(None, 24)
+        img = my_font.render(self.username, True, pygame.Color('#ffffff'))
+        window_surface.blit(img, (self.pos[0], self.pos[1] + 2*BALL_RADIUS))
+        '''
+        my_font = pygame.font.SysFont(None, 36)
+        img = my_font.render("Arch Linux", True, pygame.Color('#1793d1'))
+        window_surface.blit(img, (SCREEN_WIDTH - 175, SCREEN_HEIGHT - 200))
+
+        img = my_font.render("Ubuntu", True, pygame.Color('#e95420'))
+        window_surface.blit(img, (SCREEN_WIDTH - 175, SCREEN_HEIGHT - 170))
+
+        img = my_font.render("Fedora", True, pygame.Color('#3c6eb4'))
+        window_surface.blit(img, (SCREEN_WIDTH - 175, SCREEN_HEIGHT - 140))
+
+        img = my_font.render("Manjaro", True, pygame.Color('#34be5b'))
+        window_surface.blit(img, (SCREEN_WIDTH - 175, SCREEN_HEIGHT - 110))
+
+        img = my_font.render("Else", True, pygame.Color('#ffffff'))
+        window_surface.blit(img, (SCREEN_WIDTH - 175, SCREEN_HEIGHT - 80))
 
     pygame.display.update()
 
