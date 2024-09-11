b2a796d59334f97969a460456c5074226a83e6f9
Marc Marcos - Manjaro Desktop <marcmarcosmadruga@gmail.com>
2024-09-11 11:16:07 +0200

    deleted venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/marcmarcos/Escritorio/Proyectos Actuales/install-party-blobs/venv"
+set -gx VIRTUAL_ENV "/home/marcmarcos/Escritorio/Proyectos Actuales/install-party/install-party-blobs/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
